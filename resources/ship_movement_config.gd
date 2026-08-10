faefeb0dce5717ddc1e36eca41a4536c6cbc46aa
Brad <brad.menchl@datadoghq.com>
2026-08-09 21:48:35 -0400

    clean up, juice

--- resources/ship_movement_config.gd
+++ resources/ship_movement_config.gd
@@ -1,11 +1,11 @@
 extends Resource
 class_name ShipMovementConfig
 
-@export var acceleration: float = 400.0
-@export var deceleration: float = 200.0
+@export var acceleration: float = 2400.0
+@export var deceleration: float = 1200.0
 @export var linear_friction: float = 0.6
 @export var max_speed: float = 400.0
 
-@export var angular_acceleration: float = 8.0
+@export var angular_acceleration: float = 100.0
 @export var angular_friction: float = 4.0
 @export var max_angular_speed: float = 3.5
